24e067ac2ad97cf1357448d6bf8c9fad57708bb9
Izabella <izzwii.izabella@gmail.com>
2019-10-08 14:18:07 +0200

    sockerts

--- server/index.js
+++ server/index.js
@@ -1,30 +1,36 @@
-const five = require("johnny-five");
-const request = require("request");
-const dotenv = require("dotenv");
+const five = require('johnny-five');
+const request = require('request');
+const dotenv = require('dotenv');
 const board = new five.Board();
+const server = require('http').createServer();
+const io = require('socket.io')(server);
 
 dotenv.config();
 
+server.listen(4000);
+let minValue = 30;
+let maxValue = 70;
+
 const sendSMS = () => {
   request.post(
-    "https://api.46elks.com/a1/sms",
+    'https://api.46elks.com/a1/sms',
     {
       auth: {
         username: process.env.ELKS_USERNAME, // Specify your API username
         password: process.env.ELKS_PASSWORD // Specify your API password
       },
       form: {
-        from: "Plantan",
-        to: "+46767860086",
-        message: "VATTNA MIG"
+        from: 'Plantan',
+        to: '+46767860086',
+        message: 'VATTNA MIG'
       }
     },
     (err, res, body) => {
       if (res.statusCode == 200) {
-        console.log("Sent! The API responded:");
+        console.log('Sent! The API responded:');
         console.log(JSON.parse(body));
       } else {
-        console.log("Error:");
+        console.log('Error:');
         console.log(body);
       }
     }
@@ -33,32 +39,41 @@ const sendSMS = () => {
 
 // The board's pins will not be accessible until
 // the board has reported that it is ready
-board.on("ready", () => {
-  console.log("Ready!");
+board.on('ready', () => {
+  console.log('Ready!');
   const pump = new five.Pin(3);
-  const sensor = new five.Sensor({ pin: "A0", type: "analog" });
+  const sensor = new five.Sensor({ pin: 'A0', type: 'analog' });
   let isWatering = false;
+
   pump.low();
 
-  sensor.on("change", raw => {
+  sensor.on('change', raw => {
     let number = Math.floor((sensor.value / 1023) * 100);
     number = 100 - number;
 
-    console.log(number);
+    io.sockets.emit('percent', number);
+    io.on('connection', socket => {
+      socket.on('minValue', min => {
+        minValue = min;
+      });
+      socket.on('maxValue', max => {
+        maxValue = max;
+      });
+    });
 
-    if (number <= 30 && !isWatering) {
+    if (number <= minValue && !isWatering) {
       isWatering = true;
       pump.high();
 
       setTimeout(() => {
-        if (number < 33) {
-          sendSMS();
+        if (number < minValue + 3) {
+          // sendSMS();
           pump.low();
         }
       }, 10000);
     }
 
-    if (number >= 70 && isWatering) {
+    if (number >= maxValue && isWatering) {
       isWatering = false;
       pump.low();
     }
